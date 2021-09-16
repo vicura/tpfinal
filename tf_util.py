06ccbbdc04e4b9ed7b0a58cf3798795dec5bffcb
vicura <79314940+vicura@users.noreply.github.com>
2021-09-16 10:45:29 -0300

    Update tf_util.py

--- tf_util.py
+++ tf_util.py
@@ -6,7 +6,8 @@ Date: November 2016
 # ref: https://github.com/charlesq34/pointnet/blob/master/utils/tf_util.py
 
 import numpy as np
-import tensorflow as tf
+import tensorflow.compat.v1 as tf as tf
+tf.compat.v1.disable_eager_execution()
 
 def _variable_on_cpu(name, shape, initializer, use_fp16=False):
   """Helper to create a Variable stored on CPU memory.
