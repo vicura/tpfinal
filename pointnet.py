453b33b5c79ede29e54bf37f0b2033185b71d0a3
vicura <79314940+vicura@users.noreply.github.com>
2021-09-20 11:05:05 -0300

    Update pointnet.py

--- pointnet.py
+++ pointnet.py
@@ -65,7 +65,8 @@ class PointNet:
                                  padding='VALID', scope='maxpool')
         
         # MLP on global point cloud vector
-        net = tf.keras.layers.Flatten(net)
+        flatten_layer = tf.keras.layers.Flatten()
+        net = flatten_layer(net)
         net = tf_util.fully_connected(net, 512, bn=bn, is_training=is_training,
                                       scope='fc1', bn_decay=bn_decay)
         net = tf_util.fully_connected(net, 256, bn=bn, is_training=is_training,
