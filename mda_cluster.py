0f5447f8e4c83d3f4dd6e9614e4de00bd619b2f9
vicura <79314940+vicura@users.noreply.github.com>
2021-10-06 17:50:53 -0300

    Update mda_cluster.py

--- mda_cluster.py
+++ mda_cluster.py
@@ -111,7 +111,7 @@ def main():
         largest_cluster = max(nx.connected_component_subgraphs(G), key=len)
        
         f_summary.write("{:8.3f}{:8d}{:8d}{:8d}{:8d}{:8d}{:8d}\n".format(ts.time,len(largest_cluster),lam_atoms.shape[0],
-            hex_atoms.shape[0]other_atoms.shape[0]))
+            hex_atoms.shape[0],other_atoms.shape[0]))
 
         for node in largest_cluster:
             f_class.write("{:10d}{:8d}{:8d}\n".format(ts.frame+1,node,results[node]))
