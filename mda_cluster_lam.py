0e417fb3ce7e468c64b245a994f0b29b83bbfa6f
vicura <79314940+vicura@users.noreply.github.com>
2022-09-05 10:03:31 -0300

    Update mda_cluster_lam.py

--- mda_cluster_lam.py
+++ mda_cluster_lam.py
@@ -97,12 +97,13 @@ def main():
         
         f_summary.write("{:8.3f}{:8d}{:8d}{:8d}\n".format(ts.time,lam_atoms.shape[0],lam_ord_atoms.shape[0],desord_atoms.shape[0]))
         
-       # for atom in u.atoms:
-           # f_class.write("{:10d}{:8d}{:8d}{:^20.10f}{:^20.10f}{:^20.10f}\n".format(ts.frame,atom.index,results[atom.index],atom.position[0],atom.position[1],atom.position[2]))  #indica a que clase pertenece cada nodo del 
+        for atom in u.atoms:
+            f_class.write("{:10d}{:8d}{:8d}{:^20.10f}{:^20.10f}{:^20.10f}\n".format(ts.frame,atom.index,results[atom.index],atom.position[0],atom.position[1],atom.position[2]))  
+                                                                                       #indica a que clase pertenece cada nodo del 
                                                                                        #largest cluster
 
     f_summary.close()
-   # f_class.close()
+    f_class.close()
    
     return
 
